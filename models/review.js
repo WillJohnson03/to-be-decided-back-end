b1a2341e1c3c47532d5e37fccbb3ab759390ae4f
Wolf <d.wolftripp@gmail.com>
2022-03-24 12:18:35 -0700

    add review model

---
+++ models/review.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const reviewSchema = new Schema({
+  score: {
+    type: Number,
+    min: 1, max: 5
+  },
+  owner: {
+    type: Schema.Types.ObjectId, ref: "Profile"
+  }
+
+}, {
+  timestamps: true
+})
+
+const Review = mongoose.model('Review', reviewSchema)
+
+export {
+  Review
+}
