c10ec5eb0d17886481cb87a0baad7ec8a9cfb372
will <wbrycejohnson031086@gmail.com>
2022-03-25 10:28:20 -0400

    add sqaud model, update profile and others

--- models/video-game.js
+++ models/video-game.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema
 
 const videogameSchema = new Schema({
   name: {type: String, required: true},
-  multiplayer: Boolean
+  multiplayer: Boolean,
+  videoGameReviews: { type: mongoose.Schema.Types.ObjectId, ref: 'Review' }
 })
 
 const VideoGame = mongoose.model('Videogame', videogameSchema)
