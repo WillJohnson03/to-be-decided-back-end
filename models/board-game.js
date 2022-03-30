caa528e35a340026e1416daf23fd2b99278107e0
Wolf <d.wolftripp@gmail.com>
2022-03-29 18:18:04 -0700

    add movie and boardGame embeded models and added addMovie/addBoardgame route/controller

--- models/board-game.js
+++ models/board-game.js
@@ -2,16 +2,7 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
-const boardGameSchema = new Schema({
-  name: String,
-  min_players: Number,
-  max_players: Number,
-  descriptions: String,
-  image_url: String,
-  boardGameReviews: { type: mongoose.Schema.Types.ObjectId, ref: 'Review' }
-}, {
-  timestamps: true
-})
+
 
 const BoardGame = mongoose.model('BoardGame', boardGameSchema)
 
--- models/profile.js
+++ models/profile.js
@@ -2,17 +2,43 @@ import mongoose from 'mongoose'
 
 
 const videogameSchema = new mongoose.Schema({
-  name: {type: String, required: true},
-  // multiplayer: Boolean,
-  // videoGameReviews: { type: mongoose.Schema.Types.ObjectId, ref: 'Review' }
+  name: { type: String, required: true },
+  slug: String,
+  background_image: String,
+}, {
+  timestamps: true
+})
+
+const movieSchema = new mongoose.Schema({
+  title: String,
+  poster_path: String,
+  overview: String,
+  release_date: String,
+}, {
+  timestamps: true
+})
+
+const boardGameSchema = new mongoose.Schema({
+  name: String,
+  descriptions: String,
+  image_url: String,
+  min_players: Number,
+  max_players: Number,
+  min_playtime: Number,
+  max_playtime: Number,
+  rules_url: String,
+}, {
+  timestamps: true
 })
 
 const profileSchema = new mongoose.Schema({
-  email: {type: String, required: true, lowercase: true, unique: true},
+  email: { type: String, required: true, lowercase: true, unique: true },
   name: String,
-  videoGame: [videogameSchema]
-},{
-    timestamps: true,
+  videoGame: [videogameSchema],
+  movie: [movieSchema],
+  boardGame: [boardGameSchema]
+}, {
+  timestamps: true,
 })
 
 const Profile = mongoose.model('Profile', profileSchema)
